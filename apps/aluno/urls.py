fe0412bd3a1db8ac22350b50e273e4aea5678237
damiao96 <damiao.alves@aluno.ifsertao-pe.edu.br>
2025-03-18 21:34:29 -0300

    crud de exibe e lista

--- apps/aluno/urls.py
+++ apps/aluno/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from apps.aluno.views import index, listar, cadastrar
+from apps.aluno.views import index, listar, cadastrar, editar, exibir
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("index", index, name="index"),
     path("lista", listar, name="aluno/lista"),
-    path("cadastro", cadastrar, name="aluno/cadastro")
+    path("cadastro", cadastrar, name="aluno/cadastro"),
+    path("exibe/<int:id>", exibir, name="aluno/exibe"),
+    path("edita/<int:id>", editar, name="aluno/edita"),
 
 ]
--- apps/aluno/views.py
+++ apps/aluno/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from apps.aluno.models import Aluno
 from apps.aluno.forms import AlunoForms
 
@@ -6,10 +6,35 @@ def index(request):
     return render(request, 'aluno/index.html')
 
 def listar(request):
-    alunos = Aluno.objects.order_by("nome")
+    alunos = Aluno.objects.order_by("-id")
 
     return render(request, "aluno/lista.html", {"alunos": alunos})
 
 def cadastrar(request):
     form = AlunoForms()
+
+    if request.method == 'POST':
+        form = AlunoForms(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect("aluno/lista")
+
     return render(request, "aluno/cadastro.html", {"form": form})
+
+def editar(request, id):
+    aluno = Aluno.objects.get(id=id)
+    form = AlunoForms(instance=aluno)
+
+    if request.method == 'POST':
+        form = AlunoForms(request.POST, instance=aluno)
+        if form.is_valid:
+            form.save()
+            return redirect("aluno/lista")
+
+    return render(request, "aluno/cadastro.html", {"form": form})
+
+    return render(request, "aluno/edita.html", {"form": form,  "aluno_id": id})
+
+def exibir(request, id):
+    aluno= get_object_or_404(Aluno, pk=id)
+    return render(request, "aluno/exibe.htmkl", {"aluno": aluno})
